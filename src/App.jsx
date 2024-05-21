f70c347ef34c60494b226d8a6c57aea3d61d5cc8
bhartiPrashar <146718952+bhartiPrashar@users.noreply.github.com>
2024-05-21 11:59:15 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import ClockHeading from './Components/ClockHeading'
+import ClockSlogan from './Components/ClockSlogan'
+import './App.css'
+import CurrentTime from './Components/CurrentTime'
+
+function App() {
+  
+
+  return <div>
+    <center>
+
+    
+  <ClockHeading />
+  <ClockSlogan />
+  <CurrentTime />
+   </center>
+  </div>
+    
+  
+}
+
+export default App;
